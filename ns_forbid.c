7dfd6dae4d0d06c4d7f91cfd7f41fc19eca6c022
Austin Ellis <siniStar@IRC4Fun.net>
2016-10-09 12:28:00 -0500

    ns_forbid: fix showing up in NickServ LIST
    (Closes #5)

--- ns_forbid.c
+++ ns_forbid.c
@@ -6,12 +6,16 @@
  */
 
 #include "atheme-compat.h"
+#include "atheme.h"
+#include "authcookie.h"
+#include "../modules/nickserv/list_common.h"
+#include "../modules/nickserv/list.h"
 
 DECLARE_MODULE_V1
 (
 	"contrib/ns_forbid", false, _modinit, _moddeinit,
 	PACKAGE_STRING,
-	"Atheme Development Group <http://www.atheme.org>"
+	"Xtheme Development Group <http://www.Xtheme.org>"
 );
 
 #define FORBID_EMAIL "noemail"
@@ -20,14 +24,51 @@ static void ns_cmd_forbid(sourceinfo_t *si, int parc, char *parv[]);
 
 command_t ns_forbid = { "FORBID", "Disallows use of a nickname.", PRIV_USER_ADMIN, 3, ns_cmd_forbid, { .path = "contrib/forbid" } };
 
+static bool is_forbidden(const mynick_t *mn, const void *arg)
+{
+	myuser_t *mu = mn->owner;
+
+	return !!metadata_find(mu, "private:forbid:forbidder");
+}
+
+static bool forbid_match(const mynick_t *mn, const void *arg)
+{
+	const char *forbidpattern = (const char*)arg;
+	metadata_t *mdforbid;
+
+	myuser_t *mu = mn->owner;
+	mdforbid = metadata_find(mu, "private:forbid:reason");
+
+	if (mdforbid != NULL && !match(forbidpattern, mdforbid->value))
+		return true;
+
+	return false;
+}
+
 void _modinit(module_t *m)
 {
 	service_named_bind_command("nickserv", &ns_forbid);
+
+	use_nslist_main_symbols(m);
+
+	static list_param_t forbid;
+	forbid.opttype = OPT_BOOL;
+	forbid.is_match = is_forbidden;
+
+	static list_param_t forbid_reason;
+	forbid_reason.opttype = OPT_STRING;
+	forbid_reason.is_match = forbid_match;
+
+	list_register("forbidden", &forbid);
+	list_register("forbid-reason", &forbid_reason);
 }
 
 void _moddeinit(module_unload_intent_t intent)
 {
 	service_named_unbind_command("nickserv", &ns_forbid);
+
+	list_unregister("forbidden");
+	list_unregister("forbid-reason");
 }
 
 static void make_forbid(sourceinfo_t *si, const char *account, const char *reason)
@@ -58,9 +99,9 @@ static void make_forbid(sourceinfo_t *si, const char *account, const char *reaso
 	mu = myuser_add(account, "*", FORBID_EMAIL, MU_CRYPTPASS | MU_ENFORCE | MU_HOLD | MU_NOBURSTLOGIN);
 	mu->registered = CURRTIME;
 	mu->lastlogin = CURRTIME;
-	metadata_add(mu, "private:freeze:freezer", get_oper_name(si));
-	metadata_add(mu, "private:freeze:reason", reason);
-	metadata_add(mu, "private:freeze:timestamp", number_to_string(CURRTIME));
+	metadata_add(mu, "private:forbid:forbidder", get_oper_name(si));
+	metadata_add(mu, "private:forbid:reason", reason);
+	metadata_add(mu, "private:forbid:timestamp", number_to_string(CURRTIME));
 	if (!nicksvs.no_nick_ownership)
 	{
 		mn = mynick_add(mu, entity(mu)->name);
@@ -94,7 +135,7 @@ static void destroy_forbid(sourceinfo_t *si, const char *account)
 		return;
 	}
 
-	md = metadata_find(mu, "private:freeze:freezer");
+	md = metadata_find(mu, "private:forbid:forbidder");
 	if (md == NULL || mu->registered != mu->lastlogin ||
 			MOWGLI_LIST_LENGTH(&mu->nicks) != 1 ||
 			strcmp(mu->email, FORBID_EMAIL))
